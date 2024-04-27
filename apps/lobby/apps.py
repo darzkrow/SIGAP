252713eb9f5e3340db2c1ea7e544b1bdf5682348
DarzKrow <gfrancojf@gmail.com>
2024-04-27 12:50:01 -0400

    base

---
+++ apps/lobby/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class LobbyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.lobby'
+    verbose_name = 'GESTOR DE VISITANTE'
+    verbose_name_plural = 'GESTORES DE VISITANTES'

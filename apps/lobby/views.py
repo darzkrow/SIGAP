ee112c52e4ef1753e3c0f0914a12f8bd138b880a
DarzKrow <gfrancojf@gmail.com>
2024-04-27 14:33:22 -0400

    Agregando login

--- apps/lobby/views.py
+++ apps/lobby/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-import datetime
 from .models import Person, AccessSEDE
 from django.http import HttpResponse
 from openpyxl import Workbook
-from datetime import date
 # Create your views here.
 from .forms import SearchForm, PersonForms, AccessForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 def infoweb(request):
@@ -16,14 +14,14 @@ def infoweb(request):
     }
     return render(request,'home.html',content)
 
-#@login_required
+@login_required
 def home(request):
     pcount = Person.objects.filter(is_deleted=0).count
     acount= AccessSEDE.objects.count()
     return render(request,'sgv/index.html', {'pcount':pcount, 'acount':acount})
 
 
-#@login_required
+@login_required
 def SearchPerson(request):    
     if request.method == 'POST':
         form = SearchForm(request.POST)
@@ -42,7 +40,7 @@ def SearchPerson(request):
 
 
 
-#@login_required
+@login_required
 def Cperson(request):
     if request.method == "POST":
         form = PersonForms(request.POST, request.FILES)
@@ -52,29 +50,29 @@ def Cperson(request):
     else:
         form = PersonForms()
     return render(request, 'sgv/cPerson.html', {'form': form})
-#@login_required
+@login_required
 def PersonDetail(request,dni):
     person = get_object_or_404(Person, dni=dni)
     paccess = AccessSEDE.objects.filter(visitor=person).order_by('entry', 'hours')
     return render(request, 'sgv/pdetail.html',{'person': person, 'paccess': paccess})
 
-#@login_required
+@login_required
 def daccess(request,dni):
     person= get_object_or_404(Person, dni=dni)
     access = AccessSEDE.objects.filter(visitor=person)
     return render(request, 'sgv/daccess.html', {'access': access, 'person': person})
 
-#@login_required
+@login_required
 def detailaccess(request, access_id):
     try:
-        access = AccessSEDE.objects.get(pk=access_id)  # Use get() for single object retrieval
+        access = AccessSEDE.objects.get(pk=access_id)
     except AccessSEDE.DoesNotExist:
-        # Handle the case where access_id is not found (e.g., display an error message)
+
         return HttpResponse("Access not found")
     context = {'access': access}
     return render(request, 'sgv/detailaccess.html', context)
 
-#@login_required
+@login_required
 def Eperson(request, dni):
     person = get_object_or_404(Person, dni=dni)
     if request.method == 'POST':
@@ -88,7 +86,7 @@ def Eperson(request, dni):
 
 
 
-#@login_required
+@login_required
 def SoftdeletePerson(request, pk):
     person = get_object_or_404(Person, pk=pk)
     action_text = 'Restaurar' if person.is_deleted else 'Eliminar'
@@ -101,16 +99,16 @@ def SoftdeletePerson(request, pk):
         return redirect('lperson')  # Cambia esto a la URL deseada
     return render(request, 'sgv/softdelete.html', {'person': person, 'action_text': action_text})
 
-#@login_required
+@login_required
 def Tperson(request):
     person = Person.objects.filter(is_deleted=True)
     return render(request, 'sgv/tperson.html', {'person': person})
 
-#@login_required
+@login_required
 def lperson(request):
     person = Person.objects.filter(is_deleted=0)
     return render(request,'sgv/lperson.html', {'person':person})
-#@login_required
+@login_required
 def laccess(request):
     laccess = AccessSEDE.objects.all()
 
@@ -130,7 +128,7 @@ def laccess(request):
 #     return render(request, 'sgv/daccess.html', {'form': form})
 
 
-#@login_required
+@login_required
 def raccess(request, dni):
     person_access = get_object_or_404(Person, dni=dni)
     if request.method == "POST":
@@ -150,7 +148,7 @@ def raccess(request, dni):
     return render(request, 'sgv/raccess.html', {'form': form, 'person_access': person_access})
 
 
-#@login_required
+@login_required
 def reportexcel(request):
     accesses = AccessSEDE.objects.all().order_by('entry')
 
